5b2838a7797d89abd0c0bc342f94fd2524d830a9
raiganm <raigan.magallan@g.austincc.edu>
2025-05-15 05:26:42 +0000

    question 3

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,30 @@
-#add import
+import question_c
+
+def main():
+    while True:
+        num = input('Display Multiplication Table? (Y/N): ')
+
+        if num == 'Y' or num == 'y':
+            result = question_c.create_multiplication_table(5, 10)
+            print("Multiplication Table:")
+            question_c.display_multiplication_table(result)
+
+            while True:
+                repeat_table = input("Display multiplication table again? (Y/N): ")
+            
+                if repeat_table == 'Y' or repeat_table == 'y':
+                    print("Multiplication Table:")
+                    question_c.display_multiplication_table(result)
+                elif repeat_table == 'N' or repeat_table == 'n':
+                    print("Exiting...")
+                    return
+                else:
+                    print("Invalid Input")
+            
+        elif num == 'N' or num == 'n':
+            print("Exiting...")
+            return
+        else:
+            print("Invalid Input")
+
+main()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,24 @@
 #write functions here, don't add input('') statements here!
+def test_config():
+    return True
+
+def create_multiplication_table(rows, cols):
+    list = []
+
+    for r in range(0, rows):
+        mult_row_list = []
+
+        for c in range(0, cols):
+            mult_row_list.append((r + 1) * (c + 1))
+
+        list.append(mult_row_list)
+
+    return list
+
+def display_multiplication_table(list):
+
+    for row_list in list:
+        for item in row_list:
+            print(str(item).rjust(3, " "), end = " ")
+        
+        print(" ")
